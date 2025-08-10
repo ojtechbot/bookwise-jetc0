6506bd7de757aeb303b905a4cf929d7440ef8d10
Pomari Osain Justin <pomariosainjustin@gmail.com>
2025-08-10 11:24:11 +0000

    Improve app preloader make it more advance d

--- src/components/preloader.tsx
+++ src/components/preloader.tsx
@@ -1,19 +1,17 @@
 
 'use client';
 
-import { BookMarked } from 'lucide-react';
-
 export function Preloader() {
   return (
     <div className="fixed inset-0 bg-background z-50 flex items-center justify-center">
       <div className="relative flex flex-col items-center">
-        <div className="book">
-            <div className="book__pg-shadow"></div>
-            <div className="book__pg"></div>
-            <div className="book__pg book__pg--2"></div>
-            <div className="book__pg book__pg--3"></div>
-            <div className="book__pg book__pg--4"></div>
-            <div className="book__pg book__pg--5"></div>
+        <div className="book-preloader">
+          <div className="book-preloader__cover"></div>
+          <div className="book-preloader__pages">
+            <div className="book-preloader__page"></div>
+            <div className="book-preloader__page"></div>
+            <div className="book-preloader__page"></div>
+          </div>
         </div>
         <p className="mt-8 text-lg text-primary font-headline animate-pulse">Libroweb</p>
       </div>
