9acea2c296fad2d9b0525d645174b8354234902d
Pomari Osain Justin <pomariosainjustin@gmail.com>
2025-08-12 10:48:13 +0000

    Also update all loaders except the app reloader to be fast as the app pr

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,15 +85,10 @@ export default {
             height: '0px',
           },
         },
-        'spin-slow': {
-          '0%': { transform: 'rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg)' },
-        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'spin-slow': 'spin-slow 3s linear infinite',
       },
     },
   },
