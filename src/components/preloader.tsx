2138a5a8b1d7af247d1699f909545dc44a7ec02e
Pomari Osain Justin <pomariosainjustin@gmail.com>
2025-08-12 10:30:43 +0000

    Improve the app preloader use a normal using is out icon of a 3D book

--- src/components/preloader.tsx
+++ src/components/preloader.tsx
@@ -1,19 +1,14 @@
 
 'use client';
 
+import { Loader2 } from "lucide-react";
+
 export function Preloader() {
   return (
     <div className="fixed inset-0 bg-background z-50 flex items-center justify-center">
-      <div className="relative flex flex-col items-center">
-        <div className="book-preloader">
-          <div className="book-preloader__cover"></div>
-          <div className="book-preloader__pages">
-            <div className="book-preloader__page"></div>
-            <div className="book-preloader__page"></div>
-            <div className="book-preloader__page"></div>
-          </div>
-        </div>
-        <p className="mt-8 text-lg text-primary font-headline animate-pulse">Libroweb</p>
+      <div className="relative flex flex-col items-center gap-4">
+        <Loader2 className="w-16 h-16 text-primary animate-spin" />
+        <p className="text-lg text-primary font-headline">Libroweb</p>
       </div>
     </div>
   );
