2ab16bae9c44d3e65a668c0cdc32d98ec4734ad0
Pomari Osain Justin <pomariosainjustin@gmail.com>
2025-08-19 09:59:50 +0000

    Error: Invalid src prop (https://encrypted-tbn3.gstatic.com/shopping?q=t

--- src/next.config.ts
+++ src/next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn*.gstatic.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
