66d8c55724eba566ca9c970372c68c87a0b09dd6
Pomari Osain Justin <pomariosainjustin@gmail.com>
2025-08-19 08:06:49 +0000

    set devIndicators: false in your next.config file.

---
+++ src/next.config.ts
@@ -0,0 +1,33 @@
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestudio.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+};
+
+export default nextConfig;
